482c936bfd120e9461ee1e7fa2f29e5fcb098ef1
James Codella <jacodel@microsoft.com>
2023-07-28 09:06:46 -0400

    Adding mongo vCore Recipe tutorial

---
+++ RecipeGuide_CosmosDBMongoVCoreOpenAI/Model/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CosmosRecipeGuide
+{
+
+    public class Recipe
+    {
+        [BsonId]
+        [JsonProperty("id")]
+        public string? id { get; set; }
+
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public List<float>? embedding { get; set; }
+        public string? cuisine { get; set; }
+        public string? difficulty { get; set; }
+        public string? prepTime { get; set; }
+        public string? cookTime { get; set; }
+        public string? totalTime { get; set; }
+        public int? servings { get; set; }
+        public List<string>? ingredients { get; set; }
+        public List<string>? instructions { get; set; }
+    }     
+
+
+}
