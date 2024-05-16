02569ecb3bd5bf64ba4b811cf2a84733b63dc67a
Sandeep Nair <nair.sandeep@microsoft.com>
2024-05-16 20:17:03 +0530

    Added Cosmos DBNoSQL sample

---
+++ C#/CosmosDB-NoSQL/Service/CosmosDbService.cs
@@ -0,0 +1,279 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Cosmos.Serialization.HybridRow.Schemas;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using Container = Microsoft.Azure.Cosmos.Container;
+using PartitionKey = Microsoft.Azure.Cosmos.PartitionKey;
+
+namespace CosmosRecipeGuide.Services;
+
+/// <summary>
+/// Service to access Azure Cosmos DB for NoSQL.
+/// </summary>
+public class CosmosDbService
+{
+    private readonly Container _container;
+    CosmosClient? _client;
+    Database? _database;
+
+    /// <summary>
+    /// Creates a new instance of the service.
+    /// </summary>
+    /// <param name="endpoint">Endpoint URI.</param>
+    /// <param name="key">Account key.</param>
+    /// <param name="databaseName">Name of the database to access.</param>
+    /// <param name="containerName">Name of the container to access.</param>
+    /// <exception cref="ArgumentNullException">Thrown when endpoint, key, databaseName, or containerName is either null or empty.</exception>
+    /// <remarks>
+    /// This constructor will validate credentials and create a service client instance.
+    /// </remarks>
+    public CosmosDbService(string endpoint, string key, string databaseName, string containerName)
+    {
+        ArgumentNullException.ThrowIfNullOrEmpty(endpoint);
+        ArgumentNullException.ThrowIfNullOrEmpty(key);
+        ArgumentNullException.ThrowIfNullOrEmpty(databaseName);
+        ArgumentNullException.ThrowIfNullOrEmpty(containerName);
+
+
+        CosmosSerializationOptions options = new()
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        };
+
+        _client = new CosmosClientBuilder(endpoint, key)
+            .WithSerializerOptions(options)
+            .Build();
+
+        _database = _client?.GetDatabase(databaseName);
+        Container? container = _database?.GetContainer(containerName);
+
+
+        _container = container ??
+            throw new ArgumentException("Unable to connect to existing Azure Cosmos DB container or database.");
+    }
+
+    public async Task<bool> CheckCollectionExistsAsync()
+    {
+        try
+        {
+            // Check if the container exists
+            return await _container.ReadContainerAsync() != null;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+
+    public async Task<bool> CreateCosmosContainerAsync(string databaseName, string containerName)
+    {
+        try
+        {
+            ThroughputProperties throughputProperties = ThroughputProperties.CreateAutoscaleThroughput(1000);
+
+            // Define new container properties including the vector indexing policy
+            ContainerProperties properties = new ContainerProperties(id: containerName, partitionKeyPath: "/id")
+            {
+                // Set the default time to live for cache items to 1 day
+                DefaultTimeToLive = 86400,
+
+                // Define the vector embedding container policy
+                VectorEmbeddingPolicy = new(
+                new Collection<Embedding>(
+                [
+                    new Embedding()
+                {
+                    Path = "/vectors",
+                    DataType = VectorDataType.Float32,
+                    DistanceFunction = DistanceFunction.Cosine,
+                    Dimensions = 1536
+                }
+                ])),
+                IndexingPolicy = new IndexingPolicy()
+                {
+                    // Define the vector index policy
+                    VectorIndexes = new()
+                {
+                    new VectorIndexPath()
+                    {
+                        Path = "/vectors",
+                        Type = VectorIndexType.QuantizedFlat
+                    }
+                }
+                }
+            };
+
+            // Create the container
+            Container container = _database.CreateContainerIfNotExistsAsync(properties, throughputProperties).Result;
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+
+    public async Task<List<Recipe>> SingleVectorSearch(float[] vectors, double similarityScore)
+    {
+        
+        string queryText = @"SELECT Top 3 x.name,x.description, x.ingredients, x.cuisine,x.difficulty, x.prepTime,x.cookTime,x.totalTime,x.servings, x.similarityScore
+                            FROM (SELECT c.name,c.description, c.ingredients, c.cuisine,c.difficulty, c.prepTime,c.cookTime,c.totalTime,c.servings,
+                                VectorDistance(c.vectors, @vectors, false) as similarityScore FROM c) x
+                                    WHERE x.similarityScore > @similarityScore ORDER BY x.similarityScore desc";
+
+        var queryDef = new QueryDefinition(
+                query: queryText)
+            .WithParameter("@vectors", vectors)
+            .WithParameter("@similarityScore", similarityScore);
+
+        using FeedIterator<Recipe> resultSet = _container.GetItemQueryIterator<Recipe>(queryDefinition: queryDef);
+
+        List<Recipe> recipes = new List<Recipe>();
+        while (resultSet.HasMoreResults)
+        {
+            FeedResponse<Recipe> response = await resultSet.ReadNextAsync();
+            recipes.AddRange(response);
+        }
+        return recipes;
+    }
+
+    /// <summary>
+    /// Gets a list of all recipes where embeddings are null.
+    /// </summary>
+    public async Task<List<Recipe>> GetRecipesToVectorizeAsync()
+    {
+        QueryDefinition query = new QueryDefinition("SELECT * FROM c WHERE IS_ARRAY(c.vectors)=false");
+
+        FeedIterator<Recipe> results = _container.GetItemQueryIterator<Recipe>(query);
+
+        List<Recipe> output = new();
+        while (results.HasMoreResults)
+        {
+            FeedResponse<Recipe> response = await results.ReadNextAsync();
+            output.AddRange(response);
+        }
+        return output;
+    }
+
+    /// <summary>
+    /// Gets a count of all recipes based on embeddings status.
+    /// </summary>
+    public async Task<int> GetRecipeCountAsync(bool withEmbedding)
+    {
+        QueryDefinition query = new QueryDefinition("SELECT value Count(c.id) FROM c WHERE IS_ARRAY(c.vectors)=@status")
+            .WithParameter("@status", withEmbedding);
+
+        // Execute the query and retrieve the results
+        var queryResultSetIterator = _container.GetItemQueryIterator<int>(query);
+        var queryResultSet = await queryResultSetIterator.ReadNextAsync();
+
+        // Retrieve the count value from the results
+        return queryResultSet.FirstOrDefault();
+    }
+
+    public async Task AddRecipesAsync( List<Recipe> recipes)
+    {
+        BulkOperations<Recipe> bulkOperations = new BulkOperations<Recipe>(recipes.Count);
+        foreach (Recipe recipe in recipes)
+        {
+            bulkOperations.Tasks.Add(CaptureOperationResponse(_container.CreateItemAsync(recipe, new PartitionKey(recipe.id)), recipe));
+        }
+        BulkOperationResponse<Recipe> bulkOperationResponse = await bulkOperations.ExecuteAsync();
+    }
+
+
+    public async Task UpdateRecipesAsync(Dictionary<string, float[]> dictInput)
+    {
+        BulkOperations<Recipe> bulkOperations = new BulkOperations<Recipe>(dictInput.Count);
+        foreach (KeyValuePair<string, float[]> entry in dictInput) 
+        {
+            await _container.PatchItemAsync<Recipe>(entry.Key,new PartitionKey(entry.Key), patchOperations: new[] { PatchOperation.Add("/vectors", entry.Value)});
+        }
+    }
+
+
+    private class BulkOperations<T>
+    {
+        public readonly List<Task<OperationResponse<T>>> Tasks;
+
+        private readonly Stopwatch stopwatch = Stopwatch.StartNew();
+
+        public BulkOperations(int operationCount)
+        {
+            this.Tasks = new List<Task<OperationResponse<T>>>(operationCount);
+        }
+
+        public async Task<BulkOperationResponse<T>> ExecuteAsync()
+        {
+            await Task.WhenAll(this.Tasks);
+            this.stopwatch.Stop();
+            return new BulkOperationResponse<T>()
+            {
+                TotalTimeTaken = this.stopwatch.Elapsed,
+                TotalRequestUnitsConsumed = this.Tasks.Sum(task => task.Result.RequestUnitsConsumed),
+                SuccessfulDocuments = this.Tasks.Count(task => task.Result.IsSuccessful),
+                Failures = this.Tasks.Where(task => !task.Result.IsSuccessful).Select(task => (task.Result.Item, task.Result.CosmosException)).ToList()
+            };
+        }
+    }
+    
+
+    public class BulkOperationResponse<T>
+    {
+        public TimeSpan TotalTimeTaken { get; set; }
+        public int SuccessfulDocuments { get; set; } = 0;
+        public double TotalRequestUnitsConsumed { get; set; } = 0;
+
+        public IReadOnlyList<(T, Exception)>? Failures { get; set; }
+    }
+
+    public class OperationResponse<T>
+    {
+        public T? Item { get; set; }
+        public double RequestUnitsConsumed { get; set; } = 0;
+        public bool IsSuccessful { get; set; }
+        public Exception? CosmosException { get; set; }
+    }
+
+    private static async Task<OperationResponse<T>> CaptureOperationResponse<T>(Task<ItemResponse<T>> task, T item)
+    {
+        try
+        {
+            ItemResponse<T> response = await task;
+            return new OperationResponse<T>()
+            {
+                Item = item,
+                IsSuccessful = true,
+                RequestUnitsConsumed = task.Result.RequestCharge
+            };
+        }
+        catch (Exception ex)
+        {
+            if (ex is CosmosException cosmosException)
+            {
+                return new OperationResponse<T>()
+                {
+                    Item = item,
+                    RequestUnitsConsumed = cosmosException.RequestCharge,
+                    IsSuccessful = false,
+                    CosmosException = cosmosException
+                };
+            }
+
+            return new OperationResponse<T>()
+            {
+                Item = item,
+                IsSuccessful = false,
+                CosmosException = ex
+            };
+        }
+    }
+
+
+}
---
+++ C#/CosmosDB-NoSQL/Service/OpenAIService.cs
@@ -0,0 +1,140 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Core;
+using Microsoft.Azure.Cosmos;
+using System.Text.RegularExpressions;
+
+namespace CosmosRecipeGuide.Services;
+
+/// <summary>
+/// Service to access Azure OpenAI.
+/// </summary>
+public class OpenAIService
+{
+
+    private readonly string _openAIEmbeddingDeployment = string.Empty;
+    private readonly string _openAICompletionDeployment=string.Empty; 
+    private readonly int _openAIMaxTokens = default;
+
+    private readonly OpenAIClient? _openAIClient;
+
+    //System prompts to send with user prompts to instruct the model for chat session
+    private readonly string _systemPromptRecipeAssistant = @"
+        You are an intelligent assistant for Contoso Recipes. 
+        You are designed to provide helpful answers to user questions about using
+        recipes, cooking instructions only using the provided JSON strings.
+
+        Instructions:
+        - In case a recipe is not provided in the prompt politely refuse to answer all queries regarding it. 
+        - Never refer to a recipe not provided as input to you.
+        - If you're unsure of an answer, you can say ""I don't know"" or ""I'm not sure"" and recommend users search themselves.        
+        - Your response  should be complete. 
+        - List the Name of the Recipe at the start of your response folowed by step by step cooking instructions
+        - Assume the user is not an expert in cooking.
+        - Format the content so that it can be printed to the Command Line 
+        - In case there are more than one recipes you find let the user pick the most appropiate recipe.";
+
+    public OpenAIService(string endpoint, string key, string embeddingsDeployment, string CompletionDeployment, string maxTokens)
+    {
+
+        _openAIEmbeddingDeployment = embeddingsDeployment;
+        _openAICompletionDeployment = CompletionDeployment;
+        _openAIMaxTokens = int.TryParse(maxTokens, out _openAIMaxTokens) ? _openAIMaxTokens : 8191;
+
+
+        OpenAIClientOptions clientOptions = new OpenAIClientOptions()
+        {
+            Retry =
+            {
+                Delay = TimeSpan.FromSeconds(2),
+                MaxRetries = 10,
+                Mode = RetryMode.Exponential
+            }
+        };
+
+        try
+        {
+
+            //Use this as endpoint in configuration to use non-Azure Open AI endpoint and OpenAI model names
+            if (endpoint.Contains("api.openai.com"))
+                _openAIClient = new OpenAIClient(key, clientOptions);
+            else
+                _openAIClient = new(new Uri(endpoint), new AzureKeyCredential(key), clientOptions);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"OpenAIService Constructor failure: {ex.Message}");
+        }
+    }
+
+    public async Task<float[]?> GetEmbeddingsAsync(dynamic data)
+    {
+        try
+        {
+            EmbeddingsOptions embeddingsOptions = new()
+            {
+                DeploymentName = _openAIEmbeddingDeployment,
+                Input = { data },
+            };
+            var response = await _openAIClient.GetEmbeddingsAsync(embeddingsOptions);
+          
+            Embeddings embeddings = response.Value;
+
+            float[] embedding = embeddings.Data[0].Embedding.ToArray();
+
+            return embedding;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"GetEmbeddingsAsync Exception: {ex.Message}");
+            return null;
+        }
+    }
+
+    public async Task<(string response, int promptTokens, int responseTokens)> GetChatCompletionAsync(string userPrompt, string documents)
+    {
+
+        try
+        {
+
+            var systemMessage = new ChatRequestSystemMessage(_systemPromptRecipeAssistant + documents);
+            var userMessage = new ChatRequestUserMessage(userPrompt);
+
+
+            ChatCompletionsOptions options = new()
+            {
+                DeploymentName= _openAICompletionDeployment,
+                Messages =
+                {
+                    systemMessage,
+                    userMessage
+                },
+                MaxTokens = _openAIMaxTokens,
+                Temperature = 0.5f, //0.3f,
+                NucleusSamplingFactor = 0.95f, 
+                FrequencyPenalty = 0,
+                PresencePenalty = 0
+            };
+
+            Azure.Response<ChatCompletions> completionsResponse = await _openAIClient.GetChatCompletionsAsync(options);
+
+            ChatCompletions completions = completionsResponse.Value;
+
+            return (
+                response: completions.Choices[0].Message.Content,
+                promptTokens: completions.Usage.PromptTokens,
+                responseTokens: completions.Usage.CompletionTokens
+            );
+
+        }
+        catch (Exception ex)
+        {
+
+            string message = $"OpenAIService.GetChatCompletionAsync(): {ex.Message}";
+            Console.WriteLine(message);
+            throw;
+
+        }
+    }
+
+}
